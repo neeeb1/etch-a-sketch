b43002b56de7a321e6213b33fa2d99e2cbd59446
Ben <ben.jab.97@gmail.com>
2016-09-04 14:04:39 -0700

    added regeneration button

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 var height = 16;
 var width = 16;
 
+var $block = $('div.block');
+
 $(document).ready(function() {
 	generateGrid(height, width);
 
+	$('button').click(function() {
+		$('tr.row').remove();
+		$('td.row').remove();
 
+		height = prompt('Please enter number of rows', '16');
+		width = prompt('Please enter number of columns', '16');
+
+		generateGrid(height, width);
+	});
 });
 
 function generateGrid(height, width) {
@@ -18,4 +28,8 @@ function generateGrid(height, width) {
   	for (i = 0; i < width; i++) {
   		$('div#grid table tr.row').append('<td><div class="block"></div></td>');
   	};
+
+  	$('div.block').mouseenter(function() {
+		$(this).addClass('filled');
+	});
 };
